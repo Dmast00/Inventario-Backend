309552030994c9a7a2cdf22c98574339db1d92c0
Dmast00 <obed.hinojosa@outlook.com>
2022-07-20 12:44:43 -0500

    Se agregaron modelos

---
+++ Inventario/Models/DetalleSalidas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventario.Models
+{
+    public class DetalleSalidas
+    {
+        [Key]
+        public int IdDetalle { get; set; }
+        public int FK_IdSalida { get; set; }
+        public int FK_IdProducto { get; set; }
+        public string D_Cantidad { get; set; }
+    }
+}
---
+++ Inventario/Models/Salidas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventario.Models
+{
+    public class Salidas
+    {
+        [Key]
+        public int IdSalida { get; set; }
+        public decimal S_Total { get; set; }
+        public DateTime S_Fecha { get; set; }
+    }
+}
